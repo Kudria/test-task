2ef10e6242d75207f1882eaa72926f2b9e84f20f
Kudria <kudria15@gmail.com>
2025-07-16 21:06:51 +0300

    feat: add Task model

--- src/tasks/models.py
+++ src/tasks/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    IN_PROGRESS = 'in_progress'
+    COMPLETED = 'completed'
+
+    STATUS_CHOICES = {
+        IN_PROGRESS: 'In Progress',
+        COMPLETED: 'Completed',
+    }
+
+    text = models.TextField(
+        verbose_name="Text",
+    )
+
+    status = models.CharField(
+        verbose_name="Status",
+        max_length=16,
+        choices=STATUS_CHOICES,
+        default=IN_PROGRESS,
+    )
+
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+
+    def __str__(self):
+        return self.text[:25]
