d57d70e5befd74a08651aa62fe5e6061af267e5c
Kudria <kudria15@gmail.com>
2025-07-17 15:06:27 +0300

    feat: install drf-yasg

--- src/test_task/settings.py
+++ src/test_task/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
     'django_filters',
     'rest_framework',
+    'drf_yasg',
 
     'tasks',
 ]
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- src/test_task/urls.py
+++ src/test_task/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from tasks.views import TaskViewSet
@@ -23,7 +23,7 @@ from tasks.views import TaskViewSet
 router = DefaultRouter()
 router.register('tasks', TaskViewSet, basename='tasks')
 
+
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     path('api/', include((router.urls, 'tasks'), namespace='tasks')),
 ]
